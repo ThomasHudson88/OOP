0e5138c15b2db783e5409e250600e2d228802c5b
Thomas <Thomas_Hudson1@baylor.edu>
2024-02-26 13:24:35 -0600

    updated insects

--- InsectClass.py
+++ InsectClass.py
@@ -3,15 +3,23 @@ import random
 class Insect:
     #Creating the insect Class
 
-    def __init__(self):
-        self.legs = 4
-        self.wings = 2
-        self.flight = 1
+    def __init__(self,w,l,n):
+        self.wings = w
+        self.legs = l
+        self.flight = 0
+        self.name = n
+
 
     def calcflight(self):
         self.flight = random.randint(1, 10)
+        #get methods and accesor methods, some change a value and others just get the vlaue
+        #we have these seperate because they serve a specific func
 
     def get_miles(self):
             return self.flight
+    
+    def get_name(self):
+            return self.name
 
 #created the class, then we initialize and then we give the flight the random number
+#Always have to __init__ and list all the attributes
--- InsectProgram.py
+++ InsectProgram.py
@@ -1,15 +1,15 @@
 import InsectClass as i
 
 def main():
-    mosquito = i.Insect() 
-    housefly = i.Insect()
+    mosquito = i.Insect(2,4,'Mosquito') 
+    housefly = i.Insect(2,4,'Housefly')
     
     #instance needs to call the method
     mosquito.calcflight()
     housefly.calcflight()
 
     #print the range
-    print(f"the mosquito can fly up to {mosquito.get_miles()}")
-    print(f"the housefly can fly up to {housefly.get_miles()}")
+    print(f"the {mosquito.get_name()} can fly up to {mosquito.get_miles()}")
+    print(f"the {housefly.get_name()} can fly up to {housefly.get_miles()}")
 
 main()
